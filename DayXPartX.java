9eb5a94d6fd277e9ebb84043ca9a8c7aadb2ceef
braydonj33 <braydon.scott268@topper.wku.edu>
2023-12-06 01:48:18 -0600

    new file:   DayXPartX.java

---
+++ DayXPartX.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class DayXPartX {
+    public static void main(String[] args) throws Exception {
+        Scanner stdin = new Scanner(new File("output.txt"));
+
+        stdin.close();
+    }
+}
